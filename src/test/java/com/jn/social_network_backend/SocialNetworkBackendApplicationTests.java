b014dfe411f9b7d7c072863e374b940e55e84295
Joiney <joineyn901@gmail.com>
2025-09-23 23:22:06 -0500

    initial commit

---
+++ src/test/java/com/jn/social_network_backend/SocialNetworkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jn.social_network_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
